91df395815b510023218814db1ecd3605e4b07fb
Mikko Ronkainen <mikko@mikkoronkainen.com>
2014-09-01 14:26:38 +0300

    Updated build files

--- misc/windows/post-build-debug.bat
+++ misc/windows/post-build-debug.bat
@@ -1,13 +1,13 @@
 @echo off
 
-if not exist bin\Debug\data (
-  xcopy /E /I /Y /Q data bin\Debug\data
+if not exist %1\data (
+  xcopy /E /I /Y /Q data %1\data
 )
 
-if not exist bin\Debug\orientview.exe.config (
-  xcopy /Y /Q misc\windows\orientview.exe.config bin\Debug
+if not exist %1\orientview.exe.config (
+  xcopy /Y /Q misc\windows\orientview.exe.config %1
 )
 
-if not exist bin\Debug\opencv_core249d.dll (
-  xcopy /E /I /Y /Q debug-dll\* bin\Debug
+if not exist %1\opencv_core249d.dll (
+  xcopy /E /I /Y /Q debug-dll\* %1
 )
--- misc/windows/post-build-release.bat
+++ misc/windows/post-build-release.bat
@@ -1,9 +1,13 @@
 @echo off
 
-if not exist bin\Release\data (
-  xcopy /E /I /Y /Q data bin\Release\data
+if not exist %1\data (
+  xcopy /E /I /Y /Q data %1\data
 )
 
-if not exist bin\Release\orientview.exe.config (
-  xcopy /Y /Q misc\windows\orientview.exe.config bin\Release
+if not exist %1\orientview.exe.config (
+  xcopy /Y /Q misc\windows\orientview.exe.config %1
+)
+
+if not exist %1\readme.html (
+  xcopy /Y /Q misc\readme.html %1
 )
