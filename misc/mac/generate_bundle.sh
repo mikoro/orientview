e98e7762e2966dcc1e24b04b8e7090efb7aaeea3
Mikko Ronkainen <mikko@mikkoronkainen.com>
2014-09-02 23:27:11 +0300

    Fixed mac bundle generation

--- misc/mac/generate_bundle.sh
+++ misc/mac/generate_bundle.sh
@@ -12,5 +12,12 @@ install_name_tool -change lib/libopencv_core.2.4.dylib @executable_path/../Frame
 install_name_tool -change lib/libopencv_imgproc.2.4.dylib @executable_path/../Frameworks/libopencv_imgproc.2.4.dylib OrientView.app/Contents/MacOs/OrientView
 install_name_tool -change lib/libopencv_photo.2.4.dylib @executable_path/../Frameworks/libopencv_photo.2.4.dylib OrientView.app/Contents/MacOs/OrientView
 install_name_tool -change lib/libopencv_video.2.4.dylib @executable_path/../Frameworks/libopencv_video.2.4.dylib OrientView.app/Contents/MacOs/OrientView
+install_name_tool -change lib/libopencv_core.2.4.dylib @executable_path/../Frameworks/libopencv_core.2.4.dylib OrientView.app/Contents/Frameworks/libopencv_imgproc.2.4.dylib
+install_name_tool -change lib/libopencv_core.2.4.dylib @executable_path/../Frameworks/libopencv_core.2.4.dylib OrientView.app/Contents/Frameworks/libopencv_photo.2.4.dylib
+install_name_tool -change lib/libopencv_imgproc.2.4.dylib @executable_path/../Frameworks/libopencv_imgproc.2.4.dylib OrientView.app/Contents/Frameworks/libopencv_photo.2.4.dylib
+install_name_tool -change lib/libopencv_core.2.4.dylib @executable_path/../Frameworks/libopencv_core.2.4.dylib OrientView.app/Contents/Frameworks/libopencv_video.2.4.dylib
+install_name_tool -change lib/libopencv_imgproc.2.4.dylib @executable_path/../Frameworks/libopencv_imgproc.2.4.dylib OrientView.app/Contents/Frameworks/libopencv_video.2.4.dylib
 
 rm -rf OrientView.app/Contents/PkgInfo OrientView.app/Contents/Resources/empty.lproj
+
+otool -L OrientView.app/Contents/MacOs/OrientView
